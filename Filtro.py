0de5bf95e2ca400860cef8ba74c9d77f5bf669a3
FFBarreiro <fatimafdezbarreiro@gmail.com>
2016-10-20 11:53:29 +0200

    Engadimos os primeiros filtros

--- Filtro.py
+++ Filtro.py
@@ -16,3 +16,14 @@ columnas = ["integrated_fitCons_score",
             "phyloP7way_vertebrate",
             "phyloP20way_mammalian"]
 numeric(df,columnas)
+
+df["ExonicFunc.refGene"] = df["ExonicFunc.refGene"].astype(str)
+df["ExAC_EAS"] = pd.to_numeric(df["ExAC_EAS"])
+df["ESP6500siv2_EA"] = pd.to_numeric(df["ESP6500siv2_EA"])
+df_filter = df[(~df["ExonicFunc.refGene"].str.contains("unknown")) &
+   (~df["ExonicFunc.refGene"].str.startswith("synonymous SNV"))&
+   (~df["ExonicFunc.refGene"].str.contains("frameshift deletion|frameshift insertion"))&
+   (df["Func.refGene"].str.contains("exonic|splicing"))&
+   (df["PopFreqMax"] < 0.001)&
+   (df["ExAC_EAS"] < 0.001)&
+   (df["ESP6500siv2_EA"] < 0.001)]
