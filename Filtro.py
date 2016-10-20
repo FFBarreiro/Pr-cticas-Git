9872b16cba231ee87986392e2a7d9c54937f12e8
FFBarreiro <fatimafdezbarreiro@gmail.com>
2016-10-20 12:27:53 +0200

    Creación de funcións do filtrado

--- Filtro.py
+++ Filtro.py
@@ -1,46 +1,61 @@
 import pandas as pd
+import warnings
+warnings.filterwarnings("ignore")
 
 
-df = pd.read_table("Variants.tab", encoding = "ISO-8859-1")
+def leer_tabla():
+    """lee la tabla"""
+    df = pd.read_table("Variants.tab", encoding = "ISO-8859-1")
 
-df.replace(".",0, inplace=True)
+    df.replace(".",0, inplace=True)
+    return df
 
 def numeric(df,columnas):
     """pasar a numerico as columnas seleccionadas do dataframe"""
     for elegida in columnas:
         df[elegida]= pd.to_numeric(df[elegida])
 
-columnas = ["integrated_fitCons_score",
+def transformar_columnas(df):
+    columnas = ["integrated_fitCons_score",
             "integrated_confidence_value",
             "GERP++_RS",
             "phyloP7way_vertebrate",
             "phyloP20way_mammalian"]
-numeric(df,columnas)
+    numeric(df,columnas)
 
-df["ExonicFunc.refGene"] = df["ExonicFunc.refGene"].astype(str)
-df["ExAC_EAS"] = pd.to_numeric(df["ExAC_EAS"])
-df["ESP6500siv2_EA"] = pd.to_numeric(df["ESP6500siv2_EA"])
-df_filter = df[(~df["ExonicFunc.refGene"].str.contains("unknown")) &
-   (~df["ExonicFunc.refGene"].str.startswith("synonymous SNV"))&
-   (~df["ExonicFunc.refGene"].str.contains("frameshift deletion|frameshift insertion"))&
-   (df["Func.refGene"].str.contains("exonic|splicing"))&
-   (df["PopFreqMax"] < 0.001)&
-   (df["ExAC_EAS"] < 0.001)&
-   (df["ESP6500siv2_EA"] < 0.001)]
+def primer_filtrado():
+    df["ExonicFunc.refGene"] = df["ExonicFunc.refGene"].astype(str)
+    df["ExAC_EAS"] = pd.to_numeric(df["ExAC_EAS"])
+    df["ESP6500siv2_EA"] = pd.to_numeric(df["ESP6500siv2_EA"])
+    df_filter = df[(~df["ExonicFunc.refGene"].str.contains("unknown")) &
+       (~df["ExonicFunc.refGene"].str.startswith("synonymous SNV"))&
+       (~df["ExonicFunc.refGene"].str.contains("frameshift deletion|frameshift insertion"))&
+       (df["Func.refGene"].str.contains("exonic|splicing"))&
+       (df["PopFreqMax"] < 0.001)&
+       (df["ExAC_EAS"] < 0.001)&
+       (df["ESP6500siv2_EA"] < 0.001)]
+    return df_filter
 
-df_stopgain = df_filter[(df_filter["CADD_phred"] >= 37)&
-    (df_filter["GERP++_RS"] >= 2)&
-    (df_filter["DANN_score"]>0.995)&
-    (df_filter["DANN_score"] < 1)]
+def segundo_filtrado():
+    df_stopgain = df_filter[(df_filter["CADD_phred"] >= 37)&
+        (df_filter["GERP++_RS"] >= 2)&
+        (df_filter["DANN_score"]>0.995)&
+        (df_filter["DANN_score"] < 1)]
 
-df_splicing = df_filter[(df_filter["CADD_phred"] >= 17)&
-    (df_filter["GERP++_RS"] >= 2)&
-    (df_filter["DANN_score"]>0.995)&
-    (df_filter["DANN_score"] < 0.98)]
+    df_splicing = df_filter[(df_filter["CADD_phred"] >= 17)&
+        (df_filter["GERP++_RS"] >= 2)&
+        (df_filter["DANN_score"]>0.995)&
+        (df_filter["DANN_score"] < 0.98)]
 
-df_missense = df_filter[(df_filter["CADD_phred"] >= 17)&
-    (df_filter["GERP++_RS"] >= 2)&
-    (df_filter["DANN_score"]>0.995)&
-    (df_filter["DANN_score"] < 1)]
+    df_missense = df_filter[(df_filter["CADD_phred"] >= 17)&
+        (df_filter["GERP++_RS"] >= 2)&
+        (df_filter["DANN_score"]>0.995)&
+        (df_filter["DANN_score"] < 1)]
 
-df_filter.to_excel("Filtros.xls")
+    return [df_stopgain, df_splicing, df_missense]
+
+def generar_excel():
+    df_filter.to_excel("Filtros.xls")
+
+if __name__ == "__main__":
+    df = leer_tabla()
