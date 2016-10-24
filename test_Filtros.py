3dd28517f1620f47f54c27430d0946960f39ddcf
FFBarreiro <fatimafdezbarreiro@gmail.com>
2016-10-24 12:34:28 +0200

    Test filtro funciona correctamente (missense)

--- test_Filtros.py
+++ test_Filtros.py
@@ -10,7 +10,7 @@ class test_filtrados(unittest.TestCase):
         df = DataFrame(data=d)     
         self.assertTrue(segundo_filtrado(df))
     def test_segundo_filtrado_correcto(self):
-        d = {"DANN_score": [0.994, 0.995, 0.997, 0.999, 1, 2], 
+        d = {"DANN_score": [0.994, 0.995, 0.997, 0.999, 0.999, 2], 
              "CADD_phred": [15, 17, 20, 37, 38, 17],
              "GERP++_RS": [1, 1, 2, 2, 3, 3]}
         df = DataFrame(data=d) 
@@ -19,6 +19,9 @@ class test_filtrados(unittest.TestCase):
         self.assertEqual(sg.all(), segundo_filtrado(df)[0].values.all())
         splicing = array([20.0, 0.997, 2.0])
         self.assertEqual(splicing.all(), segundo_filtrado(df)[1].values.all())
+        missense = array([38.0, 0.999, 3.0])
+        self.assertEqual(missense.all(), segundo_filtrado(df)[2].values.all())
+        
 
 if __name__ == "__main__":
     unittest.main()
